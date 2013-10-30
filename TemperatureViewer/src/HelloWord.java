c654109f302710741192cdcb6f829eadfc1121a9
kon02686 <kon02686@BA-C-256.dslocal.com>
2013-10-30 12:58:43 +0100

    treti pokus

--- TemperatureViewer/src/HelloWord.java
+++ TemperatureViewer/src/HelloWord.java
@@ -36,6 +36,8 @@ public class HelloWord extends JFrame {
 	public HelloWord() {
 		setTitle("Hello Word");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		
