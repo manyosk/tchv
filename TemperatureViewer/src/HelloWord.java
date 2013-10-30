c352c07476eacd10ba006021c3c49e201b65fff7
kon02686 <kon02686@BA-C-256.dslocal.com>
2013-10-30 11:07:44 +0100

    Druhy pokus

--- TemperatureViewer/src/HelloWord.java
+++ TemperatureViewer/src/HelloWord.java
@@ -1,11 +1,15 @@
-import java.awt.BorderLayout;
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+import java.awt.Font;
 
 
+@SuppressWarnings("serial")
 public class HelloWord extends JFrame {
 
 	private JPanel contentPane;
@@ -30,12 +34,17 @@ public class HelloWord extends JFrame {
 	 * Create the frame.
 	 */
 	public HelloWord() {
+		setTitle("Hello Word");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblHelloWord = new JLabel("Hello Word!!");
+		lblHelloWord.setFont(new Font("Times New Roman", Font.BOLD, 23));
+		lblHelloWord.setBounds(139, 114, 172, 39);
+		contentPane.add(lblHelloWord);
 	}
-
 }
