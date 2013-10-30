5bc1cda428e3b016797582bf08b3343fe7e68678
kon02686 <kon02686@BA-C-256.dslocal.com>
2013-10-30 12:57:44 +0100

    Druhy pokus

--- TemperatureViewer/src/HelloWord.java
+++ TemperatureViewer/src/HelloWord.java
@@ -38,6 +38,8 @@ public class HelloWord extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		
+		
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
