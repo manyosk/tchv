2a0ddbaf86f1c9abc83cf0b68c2ea065a317b0af
Marian <manyo@pobox.sk>
2013-11-01 21:41:55 +0100

    - new main wnd - first FTP connect code

---
+++ TemperatureViewer/src/temperatureViewer/MainWnd.java
@@ -0,0 +1,107 @@
+package temperatureViewer;
+
+import java.awt.EventQueue;
+
+import org.apache.commons.net.ftp.*;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.net.SocketException;
+
+@SuppressWarnings("serial")
+public class MainWnd extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainWnd frame = new MainWnd();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainWnd() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 543, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 527, 21);
+		contentPane.add(menuBar);
+		
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+		
+		JMenuItem mntmClose = new JMenuItem("Close");
+		mntmClose.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false); 
+				dispose();
+			}
+		});
+		mnFile.add(mntmClose);
+		
+		JMenu mnNewMenu = new JMenu("Ftp");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmFtpSettings = new JMenuItem("Ftp settings");
+		mntmFtpSettings.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		mnNewMenu.add(mntmFtpSettings);
+		
+		JMenuItem mntmUpdate = new JMenuItem("Update files");
+		mntmUpdate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				FTPClient ftpClient = new FTPClient();
+				try {
+					ftpClient.connect("10.165.129.21");
+					int replyCode = ftpClient.getReplyCode();
+		            if (!FTPReply.isPositiveCompletion(replyCode)) {
+		                System.out.println("Operation failed. Server reply code: " + replyCode);
+		                return;
+		            }
+		            
+		            ftpClient.login("skmaryb", "GkmhajSb");
+		            replyCode = ftpClient.getReplyCode();
+		            if (!FTPReply.isPositiveCompletion(replyCode)) {
+		                System.out.println("Operation failed. Server reply code: " + replyCode);
+		                return;
+		            }
+		            
+		            
+					ftpClient.logout();
+		            ftpClient.disconnect();
+				} catch (IOException ex) {
+					System.out.println("Oops! Something wrong happened");
+		            ex.printStackTrace();
+				}
+			}
+		});
+		mnNewMenu.add(mntmUpdate);
+	}
+}
